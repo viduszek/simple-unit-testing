f4aa9eed310682f46fa3046a7b35129b26d619d4
viduszek <widuch2001@gmail.com>
2023-03-30 13:10:35 +0200

    Unified to one file. Deleting `calc.py`

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 import unittest
-from calc import Calculator
+
+
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        if b == 0:
+            raise ValueError("Cannot divide by zero")
+        return a / b
 
 
 class TestCalculator(unittest.TestCase):
